ab452e46deb20ed23aca759d36f1931de845ea24
krish203na <kanhapanchal03@gmail.com>
2024-06-20 21:22:39 +0530

    server setup changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/home/",
+  base: "/portfolio/",
   plugins: [react()],
 });
